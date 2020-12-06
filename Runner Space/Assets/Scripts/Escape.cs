deeab27d7c1c60d92d5bcc564c41c4b3a91da022
Lunelli98 <lucasklunelli@gmail.com>
2020-12-06 17:02:51 -0300

    final revision

--- Runner Space/Assets/Scripts/Escape.cs
+++ Runner Space/Assets/Scripts/Escape.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Escape : MonoBehaviour{
-	public Collider2D player;
+	public Collider2D playerCol;
+	public GameObject player;
 	public GameObject fadeOut;
 	public GameObject win;
 
 	// OnTriggerEnter2D is called when a trigger starts
 	void OnTriggerEnter2D(Collider2D col) {
-		if(col == player){
+		if(col == playerCol){
 			fadeOut.SetActive(true);
 			win.SetActive(true);
+			player.SetActive(false);
 		}
 	}
 }
--- Runner Space/Assets/Scripts/Kill.cs
+++ Runner Space/Assets/Scripts/Kill.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Kill : MonoBehaviour {
-	public Collider2D player;
+	public Collider2D playerCol;
+	public GameObject player;
 	public GameObject fadeOut;
 	public GameObject loss;
 
 	// OnTriggerEnter2D is called when a trigger starts
 	void OnTriggerEnter2D(Collider2D col) {
-		if(col == player){
+		if(col == playerCol){
 			 // Application.Quit();
 			fadeOut.SetActive(true);
 			loss.SetActive(true);
+			player.SetActive(false);
 		}
 	}
 }
